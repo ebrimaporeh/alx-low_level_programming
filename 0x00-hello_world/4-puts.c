b9c88455f4d626c109b2117a69bccf10293c82c7
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-08 20:35:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 /**
+ * main - prints a statement
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 	puts("\"programming is like building a multilingual puzzle\n");
 	return (0);
-
 }
 
