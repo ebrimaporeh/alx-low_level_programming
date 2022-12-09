60f6f00f359c1732e439f7577cab26595b091431
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-08 19:27:53 -0800

    task 4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s), sizeof(char))");
+	Printf("")
+	Printf("")
+	Printf("")
