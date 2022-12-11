5af3ef19e4ce0342201aaeb5bf23103c085e4bd8
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-10 19:57:54 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program thats prints the alphabets in lowercase in new lines
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
+
+
