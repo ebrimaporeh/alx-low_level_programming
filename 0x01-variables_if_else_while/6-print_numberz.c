422a15a8f24eb032a36b4e71b46a9142d105d32f
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-10 20:28:46 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program thats prints the alphabets in lowercase in new lines
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	for  (i = 0; i <10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);	
+}
+
+
