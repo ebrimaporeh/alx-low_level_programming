c6674a56f1140586a911403268058b442ac40a7f
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-10 20:39:38 -0800

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 
-	for  (i = 0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar((i % 10) + '0');
 	}
-	putchar('\n');
-	return (0);	
+	putchar("\n");
+	return (0);
 }
 
 
