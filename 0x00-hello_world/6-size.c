b73778462e744dcff9ab4b34cc69c7a1e763c361
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-11 16:50:08 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,25 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main -prints the sizeof various types in c
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s), sizeof(char))");
-	Printf("")
-	Printf("")
-	Printf("")
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n",
+			(unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n",
+			(unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
+
