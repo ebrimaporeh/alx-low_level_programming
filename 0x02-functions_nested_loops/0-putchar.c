61eb8c5e080ddc8e017df982f372d788111c58be
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-14 15:33:05 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- prints _putchar, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	__putchar('_');
+	__putchar('p');
+	__putchar('u');
+	__putchar('t');
+	__putchar('c');
+	__putchar('h');
+	__putchar('a');
+	__putchar('r');
+	__putchar('\n');
+
+	return (0);
+}
