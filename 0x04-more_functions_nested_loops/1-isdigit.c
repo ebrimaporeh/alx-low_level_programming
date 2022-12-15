af1985011db7983b41504a5629768485adf82bea
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-15 15:44:49 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: the digit
+ * Return: return 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 $$ c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: the character to print
+ * Return: On success 1
+ */
 
 int _putchar(char c)
 {
