43c9fccf5d4bf2ebe601a4aa8349929238dccb6b
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-15 17:54:41 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,18 +2,12 @@
 
 /**
  * mul - multiply integers
- * a - integer
- * b- integer
+ * @a: first number
+ * @b: second number
  * Return:  a mul b
  */
 
 int mul(int a, int b)
 {
-	int a;
-	int b;
-	int c;
-
-	c = a * b;
-
-	return (0);
+	return (a *I b)
 }
