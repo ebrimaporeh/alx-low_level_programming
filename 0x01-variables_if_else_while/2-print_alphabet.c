0eb534fa7221c926e5085f13fd80b3319c5c1044
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-24 17:07:57 -0800

    task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  * Return: Always 0
  */
 
-int main ();
+int main (void)
 {
-	char c;
+	char letter;
 
-	for (c >= 'a'; c <= 'z'; c++)
-		
-		putchar(c);
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
 
 	putchar('\n');
 
