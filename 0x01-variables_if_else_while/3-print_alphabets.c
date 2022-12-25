c89a9790e854c0a4cd93d7e065fe7114f0f3815c
ebrimaporeh <barry99ebrima@gmail.com>
2022-12-24 17:21:09 -0800

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints lowercase and uppercase alphabtes
+ * main- prints lowercase and uppercase alphabtes
  *
- * Return - Always 0
+ * Return: - Always 0
  */
 
 int main(void)
